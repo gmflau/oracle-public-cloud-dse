38b5543b1af289a3f46b90f3be65ad486f31293b
Gilbert Lau <gmflau@gmail.com>
2016-08-14 01:02:26 -0700

    081401

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 cloud_type=$1
-ip_addr=$2
+seed_node_ip_addr=$2
 
 echo "Input to opscenter.sh is:"
 echo cloud_type $cloud_type >> opsCenter.sh.out
-echo seed_node_ip $ip_addr >> opsCenter.sh.out
+echo seed_node_ip $seed_node_ip_addr >> opsCenter.sh.out
 
 curl -o 0.1.zip https://codeload.github.com/DSPN/install-datastax-redhat/zip/0.1
 yum -y install unzip
 unzip 0.1.zip
 cd install-datastax-redhat-0.1/bin
 
-#./opscenter.sh $cloud_type $ip_addr
+#./opscenter.sh $cloud_type $seed_node_ip_addr
