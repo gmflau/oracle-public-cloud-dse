53707bb2b848c4cecd482e66ad12e4d46101cd9b
Gilbert Lau <gmflau@gmail.com>
2016-08-14 00:38:54 -0700

    0814

--- extensions/node.sh
+++ extensions/node.sh
@@ -2,31 +2,20 @@
 
 # Install datastax cassandra node
 
-#cloud_type="occ"
-#seed_node_location=$1
-#unique_string=$2
-#data_center_name=$3
-#opscenter_location=$4
+cloud_type=$1
+seed_node_ip_addr=$2
+location=$3
+opscenter_ip_addr=$4
 
 echo "Input to node.sh is:"
-#echo cloud_type $cloud_type
-#echo seed_node_location $seed_node_location
-#echo unique_string $unique_string
-#echo opscenter_location $opscenter_location
-
-#seed_node_dns_name=
-#opscenter_dns_name=
-
-echo "Calling dse.sh with the settings:"
-#echo cloud_type $cloud_type
-#echo seed_node_dns_name $seed_node_dns_name
-#echo data_center_name $data_center_name
-#echo opscenter_dns_name $opscenter_dns_name
+echo cloud_type $cloud_type >> node.sh.out
+echo seed_node_ip_addr $seed_node_ip_addr >> node.sh.out
+echo opscenter_ip_addr $opscenter_ip_addr >> node.sh.out
 
 curl -o 0.1.zip https://codeload.github.com/DSPN/install-datastax-redhat/zip/0.1
 yum -y install unzip
 unzip 0.1.zip
 cd install-datastax-redhat-0.1/bin
 
-#./dse2.sh $cloud_type $seed_node_dns_name $data_center_name $opscenter_dns_name
+#./dse.sh $cloud_type $seed_node_ip_addr $location $opscenter_ip_addr
 
