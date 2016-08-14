3be515a35df0a09f3218c3fe27199e36daa0da04
Gilbert Lau <gmflau@gmail.com>
2016-08-14 15:52:13 -0700

    Reference to 0.1.1 release now

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -7,10 +7,10 @@ echo "Input to opscenter.sh is:"
 echo cloud_type $cloud_type >> opsCenter.sh.out
 echo seed_node_ip $seed_node_ip_addr >> opsCenter.sh.out
 
-curl -o 0.1.zip https://codeload.github.com/DSPN/install-datastax-redhat/zip/0.1
+curl -o 0.1.zip https://codeload.github.com/DSPN/install-datastax-redhat/zip/0.1.1
 yum -y install unzip
-unzip 0.1.zip
-cd install-datastax-redhat-0.1/bin
+unzip 0.1.1.zip
+cd install-datastax-redhat-0.1.1/bin
 
 ./reach_install_dse.sh
 
