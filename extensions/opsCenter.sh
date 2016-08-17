5e44eecf19acf2f28a8bfec69015de7c56708d52
Gilbert Lau <gmflau@gmail.com>
2016-08-16 22:30:56 -0700

    reer to 0.1.7

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -7,9 +7,9 @@ echo "Input to opscenter.sh is:"
 echo cloud_type $cloud_type >> opsCenter.sh.out
 echo seed_node_ip $seed_node_ip_addr >> opsCenter.sh.out
 
-curl -o 0.1.6.zip https://codeload.github.com/DSPN/install-datastax-redhat/zip/0.1.6
+curl -o 0.1.7.zip https://codeload.github.com/DSPN/install-datastax-redhat/zip/0.1.7
 yum -y install unzip
-unzip 0.1.6.zip
-cd install-datastax-redhat-0.1.6/bin
+unzip 0.1.7.zip
+cd install-datastax-redhat-0.1.7/bin
 
 ./opscenter.sh $cloud_type $seed_node_ip_addr
