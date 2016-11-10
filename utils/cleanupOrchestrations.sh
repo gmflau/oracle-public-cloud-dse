bdfb7c07bad4cd49609eb7ebae400f6eec133e2f
Gilbert Lau <gmflau@users.noreply.github.com>
2016-11-10 15:23:11 -0800

    Update cleanupOrchestrations.sh

--- utils/cleanupOrchestrations.sh
+++ utils/cleanupOrchestrations.sh
@@ -15,4 +15,13 @@ while read line
 do
     oracle-compute delete orchestration $line --force
 done < orchestrationsWithoutHeader.txt
+sleep 180
 
+# Run this the second time to ensure all orchestrations will be gone
+oracle-compute discover orchestration $OPC_USER -p $pwdFilePath > orchestrations.txt
+sed -e '1,1d' < orchestrations.txt  > orchestrationsWithoutHeader.txt
+
+while read line
+do
+    oracle-compute delete orchestration $line --force
+done < orchestrationsWithoutHeader.txt
